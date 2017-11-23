76a7442704a0e2ffedee303f6f40802db19f0997
shakehead <kaiyungli@gmail.com>
2017-11-24 02:59:51 +0800

    update contact

---
+++ content/contactus.inc.php
@@ -0,0 +1,65 @@
+<link rel="stylesheet" type="text/css" href="Styles/contactus.css">
+<p>CUSTOMER SERVICE</p>
+
+<div class="hedder">
+    <hr></hr>
+    <p>PROBLEMS</p>
+</div>
+
+<div class="title">
+    <hr></hr>
+    <p>Forgot password</p>
+</div>
+<div class="content">
+    <br>
+    <p>1. Go to Loggin page</p>
+    <p>2. Visit Forgot Password</p>
+    <p>3. Enter username and the email address</p>
+    <p>4. Select Submit</p>
+    <p>5. Check your email's box</p>
+    <p>6. Click the password reset email and enter a new password</p>
+</div>
+
+<div class="title">
+    <hr></hr>
+    <p>Profile and set up</p>
+</div>
+
+<div class="content">
+    <br>
+    <p>1. Click the user icon next to the cart icon</p>
+    <p>2. Click the Settings button</p>
+    <p>3. Update informations</p>
+    <p>4. Click the Coninue button and saved the updated information</p>
+</div>
+
+<div class="hedder">
+    <hr></hr>
+    <br><br>
+    <p>CONTACT US</p>
+</div>
+<div class="title">
+    <hr></hr>
+    <p>Email Us</p>
+</div>
+<div class="content">
+    <br>
+    <p>Please email us if you have any problems</p>
+    <p>We will answer your email within 2 business days</p>
+    <p>Email address:</p>
+    <p>echocs@gmail.com</p>
+</div>
+
+<div class="title">
+    <hr></hr>
+    <p>Call Us</p>
+</div>
+<div class="content">
+    <br>
+    <p>Please call us if you have any urgent issues</p>
+    <p>Our telephone hold time is longer than normal</p>
+    <p>Thank you for your patience</p>
+    <p>Tel number:</p>
+    <p>+852 1234 5678</p>
+</div>
+
