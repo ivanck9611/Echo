91b06024f675475406c02d27f59d785008ad0259
parkocham531 <33949251+parkocham531@users.noreply.github.com>
2017-11-25 14:17:55 +0800

    加左一頁放mouse 有6個choice

---
+++ content/productslot.inc.php
@@ -0,0 +1,55 @@
+<link rel="stylesheet" type="text/css" href="Styles/productslot.css">
+<link href="https://fonts.googleapis.com/css?family=Abel" rel="stylesheet">
+
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="img/gallery/Razer_Deathadder_Elite.png">
+      <img src="img/gallery/Razer_Deathadder_Elite.png" alt="Mountains" width="600" height="400">
+    </a>
+      <div class="desc">Razer Deathadder Elite<span style="color: red;"> Best Seller</span><br><s>$700</s>  $459</div>
+    
+  </div>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="img/gallery/g903-wireless-gaming-mouse.png">
+      <img src="img/gallery/g903-wireless-gaming-mouse.png" alt="Trolltunga Norway" width="300" height="60">
+    </a>
+    <div class="desc">LOGITECH G903 LIGHTSPEED<br>$1299</div>
+  </div>
+</div>
+
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="img/gallery/g603-lightspeed-wireless-gaming-mouse.png">
+      <img src="img/gallery/g603-lightspeed-wireless-gaming-mouse.png" alt="Forest" width="600" height="400">
+    </a>
+    <div class="desc">G603 LIGHTSPEED<br> $599</div>
+  </div>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="img/gallery/Razer_Naga_Trinity_08.png">
+      <img src="img/gallery/Razer_Naga_Trinity_08.png" alt="Northern Lights" width="600" height="400">
+    </a>
+    <div class="desc">Razer Naga Trinity<br>$799</div>
+  </div>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="img/gallery/g502-rgb-tunable-gaming-mouse.png">
+      <img src="img/gallery/g502-rgb-tunable-gaming-mouse.png" alt="Mountains" width="400" height="100">
+    </a>
+    <div class="desc">G502 PROTEUS SPECTRUM <br> $1000</div>
+  </div>
+</div>
+
+
+<div class="clearfix"></div>
+
+<div style="padding:6px;">
